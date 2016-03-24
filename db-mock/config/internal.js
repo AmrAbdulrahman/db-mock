2fb9b8d88a18aac40dfc7a18ad6ac559afb2252a
Amr Abdulrahman <abu.muslim.amr@gmail.com>
2016-03-24 12:29:07 +0200

    Fix config paths

--- db-mock/config/internal.js
+++ db-mock/config/internal.js
@@ -4,5 +4,5 @@ module.exports = {
     IDTickFile: '_id.json',
     IDStartValue: 0,
     relationsProperty: '$has',
-    userConfigFilePath: './db-mock-config.json'
+    userConfigFilePath: './db-mock/db-mock-config.json'
 };
